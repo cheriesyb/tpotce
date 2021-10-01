33f2476c177b8d487b8b485c5964946fb701d995
cheriesyb <39646410+cheriesyb@users.noreply.github.com>
2021-10-01 21:48:47 +0800

    Change Git URL and redirect directory

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -689,7 +689,7 @@ hash -r
 if ! [ "$myTPOT_DEPLOYMENT_TYPE" == "iso" ];
   then
     fuBANNER "Cloning T-Pot"
-    git clone https://github.com/telekom-security/tpotce /opt/tpot
+    git clone https://github.com/cheriesyb/tpotce.git /home/tpotUser
 fi
 
 # Let's create the T-Pot user
