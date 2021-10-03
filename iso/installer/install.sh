d62071cdd7286afc62d7f99dc1ef084043032567
cheriesyb <39646410+cheriesyb@users.noreply.github.com>
2021-10-03 18:18:41 +0800

    Remove sudo

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -721,8 +721,8 @@ echo "$mySSHPORT" | tee -a /etc/ssh/sshd_config
 sed -i '2i\auth requisite pam_succeed_if.so uid >= 1000' /etc/pam.d/cockpit
 
 # Rebuild Heimdall & Logstash
-sudo docker-compose -f /opt/tpot/docker/heimdall/docker-compose.yml build
-sudo docker-compose -f /opt/tpot/docker/elk/logstash/docker-compose.yml build
+docker-compose -f /opt/tpot/docker/heimdall/docker-compose.yml build
+docker-compose -f /opt/tpot/docker/elk/logstash/docker-compose.yml build
 
 # Let's make sure only myCONF_TPOT_FLAVOR images will be downloaded and started
 case $myCONF_TPOT_FLAVOR in
