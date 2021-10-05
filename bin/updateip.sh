73f64660c9fa1059df7260b61ff945d89d4aebaf
cheriesyb <39646410+cheriesyb@users.noreply.github.com>
2021-10-05 10:11:46 +0800

    Comment Azure key vault option

--- bin/updateip.sh
+++ bin/updateip.sh
@@ -33,7 +33,7 @@ HONEY_UUID=$myUUID
 MY_EXTIP=$myEXTIP
 MY_INTIP=$myLOCALIP
 MY_HOSTNAME=$HOSTNAME
-APP_KEY=$secret
+APP_KEY=$secret # Ensure Azure VM managed identity otherwise hardcode secret
 EOF
 chown tpot:tpot /data/ews/conf/ews.ip
 chmod 770 /data/ews/conf/ews.ip
