f88acd352f61ecbd9eed949658c010ed17cd6dd1
cheriesyb <39646410+cheriesyb@users.noreply.github.com>
2021-10-05 18:11:44 +0800

    Comment out reading secret

--- bin/updateip.sh
+++ bin/updateip.sh
@@ -6,8 +6,8 @@ source /etc/environment
 myUUID=$(lsblk -o MOUNTPOINT,UUID | grep "/" | awk '{ print $2 }')
 myLOCALIP=$(hostname -I | awk '{ print $1 }')
 myEXTIP=$(/opt/tpot/bin/myip.sh)
-accessToken=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true --silent | jq -r '.access_token')
-secret=$(curl 'https://tpotkeyvault.vault.azure.net/secrets/tpotSecret/8b4e6db37c3543f0a65264e2309935bc?api-version=2016-10-01' -H "Authorization: Bearer $accessToken" --silent | jq '.value')
+# accessToken=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true --silent | jq -r '.access_token')
+# secret=$(curl 'https://tpotkeyvault.vault.azure.net/secrets/tpotSecret/8b4e6db37c3543f0a65264e2309935bc?api-version=2016-10-01' -H "Authorization: Bearer $accessToken" --silent | jq '.value')
 if [ "$myEXTIP" = "" ];
   then
     myEXTIP=$myLOCALIP
